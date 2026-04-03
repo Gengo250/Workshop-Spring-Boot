95baf48681c64662060c4a1647da792d901364f9
Gengo250 <miguelgengo28@gmail.com>
2026-04-03 14:17:48 -0300

    JPA repositoy, dependency injection, database seeding

--- src/main/java/com/educandoweb/course/entities/User.java
+++ src/main/java/com/educandoweb/course/entities/User.java
@@ -17,7 +17,7 @@ public class User implements Serializable {
 
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
-  private long id;
+  private Long id;
   private String name;
   private String email;
   private String phone;
