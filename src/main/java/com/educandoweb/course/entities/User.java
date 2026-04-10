6e6cba36c921949dc61978f5976dd217f648679a
Gengo250 <miguelgengo28@gmail.com>
2026-04-10 08:31:16 -0300

    fix bug push

--- src/main/java/com/educandoweb/course/entities/User.java
+++ src/main/java/com/educandoweb/course/entities/User.java
@@ -1,9 +1,11 @@
 package com.educandoweb.course.entities;
 
-import java.util.List;
 import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -26,6 +28,7 @@ public class User implements Serializable {
   private String phone;
   private String password;
 
+  @JsonIgnore
   @OneToMany(mappedBy = "client")
   private List<Order> orders = new ArrayList<>();
 
@@ -40,60 +43,62 @@ public class User implements Serializable {
     this.password = password;
   }
 
-  public long getId() {
+  public Long getId() {
     return id;
   }
 
-  public String getEmail() {
-    return email;
-  }
-
-  public String getPhone() {
-    return phone;
-  }
-
-  public String getPassword() {
-    return password;
+  public void setId(Long id) {
+    this.id = id;
   }
 
   public String getName() {
     return name;
   }
 
-  public List<Order> getOrders() {
-    return orders;
-  }
-
-  public void setId(long id) {
-    this.id = id;
-  }
-
   public void setName(String name) {
     this.name = name;
+  }
+
+  public String getEmail() {
+    return email;
   }
 
   public void setEmail(String email) {
     this.email = email;
   }
 
+  public String getPhone() {
+    return phone;
+  }
+
   public void setPhone(String phone) {
     this.phone = phone;
+  }
+
+  public String getPassword() {
+    return password;
   }
 
   public void setPassword(String password) {
     this.password = password;
   }
 
-  @Override
-  public boolean equals(Object o) {
-    if (o == null || getClass() != o.getClass())
-      return false;
-    User user = (User) o;
-    return id == user.id;
+  public List<Order> getOrders() {
+    return orders;
   }
 
   @Override
   public int hashCode() {
-    return Objects.hashCode(id);
+    return Objects.hash(id);
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+    if (this == obj)
+      return true;
+    if (obj == null || getClass() != obj.getClass())
+      return false;
+    User other = (User) obj;
+    return Objects.equals(id, other.id);
   }
 }
