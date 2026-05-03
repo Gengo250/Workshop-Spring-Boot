3de9481c38d92f47c78e47701a2b71f104d4865b
Gengo250 <miguelgengo28@gmail.com>
2026-05-03 18:09:10 -0300

    fix

--- src/main/java/com/educandoweb/course/resources/UserResource.java
+++ src/main/java/com/educandoweb/course/resources/UserResource.java
@@ -1,4 +1,3 @@
-
 package com.educandoweb.course.resources;
 
 import java.util.List;
@@ -9,12 +8,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 import com.educandoweb.course.entities.User;
 import com.educandoweb.course.services.UserService;
 
 @RestController
 @RequestMapping(value = "/users")
-class UserResource {
+public class UserResource {
 
   @Autowired
   private UserService service;
@@ -27,8 +27,7 @@ class UserResource {
 
   @GetMapping(value = "/{id}")
   public ResponseEntity<User> findById(@PathVariable Long id) {
-    User obj = service.findByID(id);
+    User obj = service.findById(id);
     return ResponseEntity.ok().body(obj);
   }
-
 }
--- src/main/java/com/educandoweb/course/services/UserService.java
+++ src/main/java/com/educandoweb/course/services/UserService.java
@@ -6,8 +6,8 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.educandoweb.course.repositories.UserRepository;
 import com.educandoweb.course.entities.User;
+import com.educandoweb.course.repositories.UserRepository;
 
 @Service
 public class UserService {
@@ -19,7 +19,7 @@ public class UserService {
     return repository.findAll();
   }
 
-  public User findByID(Long id) {
+  public User findById(Long id) {
     Optional<User> obj = repository.findById(id);
     return obj.get();
   }
